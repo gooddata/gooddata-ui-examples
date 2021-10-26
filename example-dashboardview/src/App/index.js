c12641f225269951e76d4ed9b2dcfefdec7d4ae6
Jiri Zajic <jiri.zajic@mensa.cz>
2021-10-26 11:57:53 -0700

    FEATURE: MKB-3182 Add DashboardView example

---
+++ example-dashboardview/src/App/index.js
@@ -0,0 +1,30 @@
+// (C) 2020 GoodData Corporation
+import React from "react";
+import * as Md from "../md";
+import Hint from "./Hint";
+import { DashboardView } from "@gooddata/sdk-ui-ext";
+
+// Try editing lines below 👇
+const dashboard = "aaAxaLgZdXxf";
+//const dashboard = Md.Dashboards._2ProductDetails;
+
+export default () => (
+  <>
+    <h1>Embedding using &lt;DashboardView /&gt;</h1>
+
+    <div class="dashboard">
+      <DashboardView
+        dashboard={dashboard}
+        config={{
+          // You will need your Mapbox token to display geocharts correctly
+          // https://docs.mapbox.com/help/getting-started/access-tokens/
+          // mapboxToken: ''
+        }}
+      />
+    </div>
+
+    <hr />
+
+    <Hint />
+  </>
+);
