1912d0a01ebd6b7f3f7359564327542f79c2f777
hackerstanislav <hacker.stanislav@gmail.com>
2022-08-08 14:46:52 +0200

    RAIL-4365 - Dependencies deduplication in interactive examples SDK upgrade script
    JIRA: RAIL-4365

--- scripts/bump-sdk.sh
+++ scripts/bump-sdk.sh
@@ -2,4 +2,4 @@
 
 # go to every example- directory and upgrade all packages in @gooddata scope
 # since there are unlikely to be any non-sdk packages used, this is good enough (no need for explicit package lists)
-find . -name "example-*" -type d -exec bash -c "cd '{}' && yarn upgrade --scope '@gooddata' --latest" \;
+find . -name "example-*" -type d -exec bash -c "cd '{}' && yarn upgrade --scope '@gooddata' --latest && npx yarn-deduplicate" \;
